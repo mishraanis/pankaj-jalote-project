a983fea8e55633133903943aff2460d01e5849d1
mishraanis <mishraanis@gmail.com>
2023-02-04 12:21:16 +0530

    new changes 100% done

--- src/AboutTheAuthor.js
+++ src/AboutTheAuthor.js
@@ -5,8 +5,8 @@ export default function AboutTheAuthor() {
             <h1 className='font-sans font-bold text-center text-3xl py-3 md:pt-5 md:pb-2'>About the Author</h1>
             <div className='w-full flex justify-evenly 3xl:w-10/12 items-center font-sans font-semibold'>
                 <img src={PankajJalote} alt='pankaj_jalote_image' className='rounded-[3rem] w-2/12'/>
-                <div className='w-9/12 md:w-7/12 text-base md:text-lg 3xl:text-xl'>
-                    <span className='text-2xl text-violet-800'>Pankaj Jalote</span> was the founding Director of IIIT-Delhi (from 2008 to 2018),
+                <div className='w-9/12 md:w-7/12 text-base md:text-base 3xl:text-xl'>
+                    <span className='text-xl text-violet-800'>Pankaj Jalote</span> was the founding Director of IIIT-Delhi (from 2008 to 2018),
                     which is now a highly-respected institution globally with high quality research and education, and has been ranked in BRICS
                     top 200 universities. Prior to this, he has been a Chair Professor at IIT Delhi, Professor and head of CS Department,
                     IIT Kanpur, and Assistant Professor at University of Maryland at college Park. He was also Vice President at Infosys for
